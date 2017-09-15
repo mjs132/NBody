b26037a9b4a1ed0d32288598bc33193495ae9449
Max Schettewi <mj132@duke.edu>
2017-09-15 18:08:29 -0400

    initial commit

---
+++ NBody.java
@@ -0,0 +1,94 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class NBody {
+	
+	public static double readRadius(String fname) {
+		double rad = 0;
+		try {
+				Scanner scan = new Scanner(new File(fname));
+				scan.nextInt();
+				rad = scan.nextDouble();
+				scan.close();
+				return rad;
+			} catch (FileNotFoundException e){
+				System.out.println("File not found");
+				System.exit(0);
+			}
+			return 0;
+	}
+	
+	public static Planet[] readPlanets(String fname) {
+		try {
+			Scanner scan = new Scanner(new File(fname));
+			int numOfPlanets = scan.nextInt();
+			scan.nextDouble();
+			Planet[] planets = new Planet[numOfPlanets];
+			for(int i = 0; i < planets.length; i++) {
+				double XPos = scan.nextDouble();
+				double YPos = scan.nextDouble();
+				double XVel = scan.nextDouble();
+				double YVel = scan.nextDouble();
+				double Mass = scan.nextDouble();
+				String FileName = scan.next();
+				planets[i] = new Planet(XPos, YPos, XVel, YVel, Mass, FileName);
+			}
+			scan.close();
+			return planets;
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+			System.exit(0);
+			return null;
+		}
+	}
+	
+	public static void main(String[] args){
+		double totalTime = 157788000.0;
+		double dt = 25000.0;
+		String pfile = "data/planets.txt";
+		if (args.length > 2) {
+			totalTime = Double.parseDouble(args[0]);
+			dt = Double.parseDouble(args[1]);
+			pfile = args[2];
+		}
+		
+		String fname= "./data/planets.txt";
+		//Planet[] planets = null; // readPlanets(fname);
+		Planet[] planets = readPlanets(fname);
+		//double radius = 0.0; // readRadius(fname);
+		double radius = readRadius(fname);
+		
+		StdDraw.setScale(-radius, radius);
+		StdDraw.picture(0,0,"images/starfield.jpg");
+		for(Planet p : planets) {
+			Planet.draw(p.myXPos, p.myYPos, p.myFileName);
+		}
+		for(double t = 0.0; t < totalTime; t += dt) {
+			double[] xForces = new double[planets.length];
+			double[] yForces = new double[planets.length];
+			
+			for(int i = 0; i < planets.length; i++) {
+				xForces[i] = planets[i].calcNetForceExertedByX(planets);
+				yForces[i] = planets[i].calcNetForceExertedByY(planets);
+			}
+			
+			for(int i = 0; i < planets.length; i++) {
+				planets[i].update(dt, xForces[i], yForces[i]);
+			}
+			StdDraw.picture(0,0,"images/starfield.jpg");
+			for(Planet p : planets) {
+				Planet.draw(p.myXPos, p.myYPos, p.myFileName);
+			}
+			StdDraw.show(10);
+		}
+		System.out.printf("%d\n", planets.length);
+		System.out.printf("%.2e\n", radius);
+		for (int i = 0; i < planets.length; i++) {
+		    System.out.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+		   		              planets[i].myXPos, planets[i].myYPos, 
+		                      planets[i].myXVel, planets[i].myYVel, 
+		                      planets[i].myMass, planets[i].myFileName);	
+		}
+	}
+}
---
+++ Planet.java
@@ -0,0 +1,116 @@
+public class Planet {
+	double myXPos;
+	double myYPos;
+	double myXVel;
+	double myYVel;
+	double myMass;
+	String myFileName;
+	
+public Planet(Planet p) {
+	myXPos = p.myXPos;
+	myYPos = p.myYPos;
+	myXVel = p.myXVel;
+	myYVel = p.myYVel;
+	myMass = p.myMass;
+	myFileName = p.myFileName;
+}
+	
+	public Planet(double xp, double xy, double xv, double yv, double mass, String filename) {
+		this.myXPos = xp;
+		this.myYPos = xy;
+		this.myXVel = xv;
+		this.myYVel = yv;
+		this.myMass = mass;
+		this.myFileName = filename;
+	}
+	
+	public double calcDistance(Planet pp) {
+		double x1 = this.myXPos;
+		double y1 = this.myYPos;
+		double x2 = pp.myXPos;
+		double y2 = pp.myYPos;
+		double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		if(distance < 0) {
+			distance += distance * -2;
+		}
+		return distance;
+	}
+	
+	public double calcForceExertedBy(Planet pp) {
+		double r = this.calcDistance(pp);
+		double G = 6.67e-11;
+		double force = G * this.myMass * pp.myMass/(r*r);
+		/*if(force < 0) {
+			force += force * -2;
+		}*/
+		return force;
+	}
+	
+	//put all variable to lower case...add my variable in front?
+	public double calcForceExertedByX(Planet pp) {
+		double force = this.calcForceExertedBy(pp);
+		double distanceX = this.myXPos - pp.myXPos;
+		//what about negative???
+		double distance = this.calcDistance(pp);
+		double forceX = force * distanceX / distance;
+		if(forceX < 0) {
+			forceX += forceX * -2;
+		}
+		return forceX;
+	}
+	
+	public double calcForceExertedByY(Planet pp) {
+		double force = this.calcForceExertedBy(pp);
+		double distanceY = this.myYPos - pp.myYPos;
+		double distance = this.calcDistance(pp);
+		double forceY = force * distanceY / distance;
+		if(forceY < 0) {
+			forceY += forceY * -2;
+		}
+		return forceY;
+	}
+
+	//not working yet....might need to know if force is positive or negative
+	public double calcNetForceExertedByX(Planet[] allPlanets){
+		double forceX = 0;
+		for (Planet p : allPlanets) {
+			if(!p.equals(this)) {
+				if(p.myXPos > this.myXPos) {
+					forceX += this.calcForceExertedByX(p);
+				}
+				else {
+					forceX -= this.calcForceExertedByX(p);
+				}
+			}
+		}
+		return forceX;
+	}
+	
+	public double calcNetForceExertedByY(Planet[] allPlanets) {
+		double forceY = 0; 
+		for (Planet p : allPlanets) {
+			if(!p.equals(this)) {
+				if(p.myYPos > this.myYPos) {
+					forceY += this.calcForceExertedByY(p);
+				}
+				else {
+					forceY -= this.calcForceExertedByY(p);
+				}
+			}
+		}
+		return forceY;
+	}
+	
+	public void update(double seconds, double xforce, double yforce) {
+		double accelX = xforce / this.myMass;
+		double accelY = yforce / this.myMass;
+		this.myXVel += seconds * accelX;
+		this.myYVel += seconds * accelY;
+		this.myXPos += seconds * this.myXVel;
+		this.myYPos += seconds * this.myYVel;
+	}
+	
+	public static void draw(double myXPos, double myYPos, String myFileName) {
+		StdDraw.picture(myXPos, myYPos,"images/"+myFileName);
+	}
+}
